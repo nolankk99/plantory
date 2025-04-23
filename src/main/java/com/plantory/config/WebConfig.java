15d985296aad4f758a2d11abfcb201bd8cf3e9d6
nolankk99 <138542767+nolankk99@users.noreply.github.com>
2025-04-23 13:26:22 +0900

    웹 설정 클래스 추가

---
+++ src/main/java/com/plantory/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.plantory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+                
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
