dd19085ec1a70caaeb0e15ff71f1783a4ad5dd34
nolankk99 <138542767+nolankk99@users.noreply.github.com>
2025-04-23 13:25:23 +0900

    메인 애플리케이션 클래스 추가

---
+++ src/main/java/com/plantory/PlantoryApplication.java
@@ -0,0 +1,12 @@
+package com.plantory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantoryApplication.class, args);
+    }
+}
