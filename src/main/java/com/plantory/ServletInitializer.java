a80be18c9e4a1e728a1b81ad4f0a3ed2f65858b8
nolankk99 <138542767+nolankk99@users.noreply.github.com>
2025-04-23 13:25:33 +0900

    서블릿 이니셜라이저 추가

---
+++ src/main/java/com/plantory/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.plantory;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlantoryApplication.class);
+    }
+}
